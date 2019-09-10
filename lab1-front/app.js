e8f1a2542315510481ad86e8d34b6aa14fb036a7
Higor Santos <higor.dantas@ccc.ufcg.edu.br>
2019-09-09 21:44:20 -0300

    correção no post

--- lab1-front/app.js
+++ lab1-front/app.js
@@ -2,20 +2,23 @@ let URL = 'https://lab01-projsw-ufcg.herokuapp.com/api/disciplinas';
 let $table = document.querySelector('table');
 
 function fetch_disciplinas() {
-    $table.innerHTML = '<tbody><tr><th>Nome</th><th>Nota</th><th>Apagar?</th></tr></tbody>'
+    $table.innerHTML = '<thead><tr><th>Nome</th><th>Nota</th><th>Apagar?</th></tr></thead>';
 
     fetch(URL)
-    .then(r => r.json())
-    .then(dado => {
-        dado.forEach(disciplina => {
-            createTable(disciplina);
+        .then(r => r.json())
+        .then(dado => {
+            dado.forEach(disciplina => {
+                criaTabela(disciplina);
+            });
         });
-    });
 }
 
 function save() {
-    let nome = document.querySelector('#nome').value;
-    let nota = document.querySelector('#nota').value;
+    let $inputNome = document.querySelector('#nome');
+    let $inputNota = document.querySelector('#nota');
+
+    let nome = $inputNome.value;
+    let nota = $inputNota.value;
 
     let disciplina = `{
         "nome":"${nome}", 
@@ -30,39 +33,38 @@ function save() {
         'method': 'POST',
         'body': disciplina,
         'headers': header
-    })
-    .then(fetch_disciplinas);
+    }).then(_ => {
+        $inputNome.value = '';
+        $inputNota.value = '';
+        fetch_disciplinas();
+    });
 }
 
-function removeAll() {
-    let $trs = $table.querySelectorAll('tr');
-    console.log($trs)
-}
+function remove() {
+    let $tr = this.parentNode.parentNode;
+    let $id = $tr.id;
 
-function remove(self) {
-    let $id = parseInt(self.path[2].id, 10);
-    let $tr = self.srcElement.parentNode.parentNode;
-    
     fetch(URL + `/${$id}`, {
         'method': 'DELETE'
     })
-    .then($tr.parentNode.removeChild($tr));
+    .then($table.removeChild($tr));
 }
 
-function createTable(disciplina) {
+function criaTabela(disciplina) {
     let $tr = document.createElement('tr');
     let $tdNome = document.createElement('td');
     let $tdNota = document.createElement('td');
     let $tdApagar = document.createElement('td');
     let $button = document.createElement('button');
-    
+
     $tr.id = disciplina.id;
     $tdNome.innerText = disciplina.nome;
     $tdNota.innerText = disciplina.nota;
+
     $button.innerText = 'Apagar';
     $button.setAttribute('type', 'submit');
     $button.addEventListener('click', remove);
-    
+
     $tdApagar.appendChild($button);
     $tr.appendChild($tdNome);
     $tr.appendChild($tdNota);
