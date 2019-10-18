d59e232e74c2fb03d40ff7d580f3e8b5ed5093a7
Higor Santos <higor.dantas@ccc.ufcg.edu.br>
2019-10-17 23:22:29 -0300

    feito testes, falta corrigir erros com uso de includes em array

--- lab2-front/scoord.js
+++ lab2-front/scoord.js
@@ -12,7 +12,9 @@ function disciplina(id, nome, creditos, pre_requisito) {
     
     d.creditos = creditos;
     d.pre_requisitos = pre_requisito;
+
     d.id = () => id;
+
     d.get_nome = () => _nome;
     d.set_nome = function (novo_nome) {
         _nome = novo_nome;
@@ -31,6 +33,7 @@ function turma(disciplina, periodo) {
     let _estudantes = [];
 
     t.get_disciplina = () => _disciplina;
+
     t.get_periodo = () => _periodo;
     t.set_periodo = function (novo_periodo) {
         _periodo = novo_periodo;
@@ -40,7 +43,7 @@ function turma(disciplina, periodo) {
     t.set_professor = function (professor) {
         _professor = professor;
     }
-    
+
     t.get_estudantes = () => _estudantes;
 
     t.matricular_estudante = function (estudante) {
@@ -51,9 +54,12 @@ function turma(disciplina, periodo) {
     }
 
     t.desmatricular_estudante = function (estudante) {
-        if ((_status == situacao.P || _status == situacao.A) && 
-                !_estudantes.includes(estudante)) {
-            _estudantes.pop(estudante);
+        if ((_status == situacao.P || _status == situacao.A)) {
+            _estudantes.forEach(function (item, index, arr) {
+                if (estudante.get_matricula() === item.get_matricula()) {
+                    arr.splice(index, 1);
+                }
+            });
         }
     }
 
@@ -99,15 +105,68 @@ function professor(matricula, nome, email, cpf, url_foto) {
     }
 
     p.aloca_turma = function (turma) {
-        _turmas.push(turma);
+        if (!_turmas.includes(turma)) {
+            _turmas.push(turma);
+        }
     }
+
     p.turmas = function (semestre) {
         return _turmas.filter((elemento) => semestre === elemento.get_periodo());
     }
+
     p.get_turmas = () => _turmas;
+
+    return p;
 }
+
+
+function estudante(matricula, nome, email, cpf, url_foto) {
+    let _matricula = matricula;
+    let _nome = nome;
+    let _email = email;
+    let _cpf = cpf;
+    let _url_foto = url_foto;
+    let _turmas = [];
+
+    let e = {};
+
+    e.get_nome = () => _nome;
+    e.set_nome = function (novo_nome) {
+        _nome = novo_nome;
+    }
+
+    e.get_matricula = () => _matricula;
+
+    e.get_email = () => _email;
+    e.set_email = function (novo_email) {
+        _email = novo_email;
+    }
+
+    e.get_cpf = () => _cpf;
+
+    e.get_url_foto = () => _url_foto;
+    e.set_url_foto = function (nova_url) {
+        _url_foto = nova_url;
+    }
+
+    e.matricula = function (turma) {
+        if (!_turmas.includes(turma)) {
+            _turmas.push(turma);
+        }
+    }
+
+    e.turmas = function (semestre) {
+        return _turmas.filter((elemento) => semestre === elemento.get_periodo());
+    }
+    e.get_turmas = () => _turmas;
+
+    return e;
+}
+
 
 module.exports = {
     disciplina, 
-    turma
+    turma,
+    professor,
+    estudante
 }
--- lab2-front/test.js
+++ lab2-front/test.js
@@ -1,13 +1,15 @@
 let assert = require('assert');
 let disciplina = require('./scoord').disciplina;
 let turma = require('./scoord').turma;
+let professor = require('./scoord').professor;
+let estudante = require('./scoord').estudante;
 
 describe('factory Disciplina', function() {
     let d0;
 
     before(async () => {
         d0 = disciplina('prog1', 'Programação 1', 4, []);
-    })
+    });
 
     it('deve criar disciplinas distintas a cada invocação', function(){
         d0 = disciplina('prog1', 'Programação 1', 4, []);
@@ -44,15 +46,21 @@ describe('factory Disciplina', function() {
 describe('factory Turma', function () {
     let t1;
     let d1;
+    let e1, e2, e3, e4;
 
     before (async () => {
         d1 = disciplina('prog1', 'Programação 1', 4, []);
-        t1 = turma(d1, "1");
+        t1 = turma(d1, '1');
+        e1 = estudante('11111', 'João', 'joao@yahoo.com', '1234567890', 'https://facebook.com/joao/perfil.jpg');
+        e2 = estudante('12345', 'Maria', 'maria@gmail.com', '14725836902', 'https://twitter.com/maria/foto-perfil.jpg');
+        e3 = estudante('12255', 'Pedro', 'pedro@hotmail.com', '36925814775', 'http://linkedin.com/pedro/foto_do_perfil.jpg');
+        e4 = estudante('15279', 'Ana', 'ana@gmail.com', '45678912337', 'http://linkedin.com/ana/fotinha.jpg');
     });
 
     it ('deve criar turmas diferentes a cada invocação', function () {
         t2 = turma(d1, "1");
         t3 = turma(d1, "1");
+        
         assert.notEqual(t1, t2);
         assert.notEqual(t1, t3);
         assert.notEqual(t2, t3);
@@ -65,4 +73,187 @@ describe('factory Turma', function () {
         assert.deepStrictEqual([], t1.get_estudantes());
         assert.equal('planejada', t1.get_status());
     });
-})
+
+    it ('deve permitir atualização do período e professor', function () {
+        p1 = professor('201415145', 'Sei lá', 'seila@computacao.ufcg.edu.br', '32145698703', 'http://linkedin.com/seila/foto_do_perfil.jpg');
+        
+        t1.set_periodo('2');
+        t1.set_professor(p1);
+        assert.equal('2', t1.get_periodo());
+        assert.equal(p1, t1.get_professor());
+    });
+
+    it ('deve ser possível matricular e estudantes e estes são únicos', function () {
+        t1.matricular_estudante(e1);
+        assert.equal(1, t1.get_estudantes().length);
+        t1.matricular_estudante(e1);
+        assert.equal(1, t1.get_estudantes().length);
+        
+        t1.matricular_estudante(e3);
+        assert.equal(2, t1.get_estudantes().length);
+        t1.matricular_estudante(e3);
+        assert.equal(2, t1.get_estudantes().length);
+
+        t1.matricular_estudante(e2);
+        assert.equal(3, t1.get_estudantes().length);
+    });
+
+    it ('só deve ser possível matricular estudantes se o status da turma for planejada ou ativa', function () {
+        assert.equal(3, t1.get_estudantes().length);
+        t1.set_status('concluida');
+        t1.matricular_estudante(e4);
+        assert.equal(3, t1.get_estudantes().length);
+        t1.set_status('ativa');
+        t1.matricular_estudante(e4);
+        assert.equal(4, t1.get_estudantes().length);
+    });
+
+    it ('ao setar um novo status inválido, o mesmo não valerá', function () {
+        t1.set_status('em andamento');
+        assert.equal('ativa', t1.get_status());
+    });
+
+    it ('deve ser possível desmatricular um aluno', function () {
+        t1.desmatricular_estudante(e3);
+        assert.equal(3, t1.get_estudantes().length);
+        t1.desmatricular_estudante(e2);
+        assert.equal(2, t1.get_estudantes().length);
+        t1.desmatricular_estudante(e1);
+        assert.equal(1, t1.get_estudantes().length);
+        t1.desmatricular_estudante(e4);
+        assert.equal(0, t1.get_estudantes().length);
+    });
+});
+
+
+describe('factory Professor', function () {
+    let p1, d1, d2, d3, t1, t2, t3;
+
+    before(async () => {
+        p1 = professor('22314587', 'Raquel', 'raquel@computacao.ufcg.edu.br', '12345674106', 'http://linkedin.com/raquel/foto.jpg');
+        d1 = disciplina('prog1', 'Programação 1', 4, []);
+        d2 = disciplina('prog2', 'Programação 2', 4, []);
+        d3 = disciplina('eda', 'Estrutura de Dados', 4, []);
+        t1 = turma(d1, '1');
+        t2 = turma(d2, '2');
+        t3 = turma(d3, '3');
+    });
+
+    it('deve criar professores diferentes a cada invocação', function () {
+        p2 = professor('26568501', 'Dalton', 'dalton@computacao.ufcg.edu.br', '07728061581', 'http://linkedin.com/dalton/foto.jpg');
+        p3 = professor('55727296', 'Matheus', 'matheus@computacao.ufcg.edu.br', '48375757376', 'http://linkedin.com/matheus/foto.jpg');
+        
+        assert.notEqual(p1, p2);
+        assert.notEqual(p1, p3);
+        assert.notEqual(p2, p3);
+    });
+
+    it('deve reter dados da inicialização', function () {
+        assert.equal('Raquel', p1.get_nome());
+        assert.equal('22314587', p1.get_matricula());
+        assert.equal('raquel@computacao.ufcg.edu.br', p1.get_email());
+        assert.equal('12345674106', p1.get_cpf());
+        assert.equal('http://linkedin.com/raquel/foto.jpg', p1.get_url_foto());
+        assert.deepStrictEqual([], p1.get_turmas());
+    });
+
+    it('deve ser possível alterar nome, email e url da foto', function() {
+        p1.set_nome('Lívia');
+        p1.set_email('livia@computacao.ufcg.edu.br');
+        p1.set_url_foto('http://linkedin.com/raquel/foto.jpg');
+        assert.equal('Lívia', p1.get_nome());
+        assert.equal('livia@computacao.ufcg.edu.br', p1.get_email());
+        assert.equal('http://linkedin.com/raquel/foto.jpg', p1.get_url_foto())
+    });
+
+    it('não deve ser possível alterar matrícula e cpf', function () {
+        assert.throws(function () {
+            p1.set_matricula('14796');
+        }, TypeError);
+
+        assert.throws(function () {
+            p1.set_cpf('00000000000');
+        }, TypeError);
+    });
+
+    it('deve ser possível alocar turmas', function () {
+        p1.aloca_turma(t1);
+        assert.equal(1, p1.get_turmas().length);
+        p1.aloca_turma(t2);
+        assert.equal(2, p1.get_turmas().length);
+        p1.aloca_turma(t3);
+        assert.equal(3, p1.get_turmas().length);
+        p1.aloca_turma(t1);
+        assert.equal(3, p1.get_turmas().length);
+    });
+
+    it('deve ser possível filtrar as turmas vinculadas por semestre', function () {
+        assert.deepStrictEqual([t1], p1.turmas('1'));
+        assert.deepStrictEqual([t2], p1.turmas('2'));
+        assert.deepStrictEqual([t3], p1.turmas('3'));
+    });
+});
+
+
+describe('factory Estudante', function () {
+    let e1, d1, d2, d3, t1, t2, t3;
+
+    before(async () => {
+        e1 = estudante('11111', 'João', 'joao@yahoo.com', '1234567890', 'https://facebook.com/joao/perfil.jpg');
+        d1 = disciplina('prog1', 'Programação 1', 4, []);
+        d2 = disciplina('prog2', 'Programação 2', 4, []);
+        d3 = disciplina('eda', 'Estrutura de Dados', 4, []);
+        t1 = turma(d1, '1');
+        t2 = turma(d2, '2');
+        t3 = turma(d3, '3');
+    });
+
+    it('deve criar estudantes diferentes a cada invocação', function () {
+        e2 = estudante('12345', 'Maria', 'maria@gmail.com', '14725836902', 'https://twitter.com/maria/foto-perfil.jpg');
+        e3 = estudante('12255', 'Pedro', 'pedro@hotmail.com', '36925814775', 'http://linkedin.com/pedro/foto_do_perfil.jpg');
+
+        assert.notEqual(e1, e2);
+        assert.notEqual(e1, e3);
+        assert.notEqual(e2, e3);
+    });
+
+    it('deve reter os dados da inicialização', function () {
+        assert.equal('João', e1.get_nome());
+        assert.equal('11111', e1.get_matricula());
+        assert.equal('joao@yahoo.com', e1.get_email());
+        assert.equal('1234567890', e1.get_cpf());
+        assert.equal('https://facebook.com/joao/perfil.jpg', e1.get_url_foto());
+        assert.deepStrictEqual([], e1.get_turmas());
+        assert.equal();
+    });
+
+    it('deve ser possível alterar nome', function() {
+        e1.set_nome('Henrique');
+        assert.equal('Henrique', e1.get_nome());
+    });
+
+    it('não deve ser possível alterar matrícula e cpf', function () {
+        assert.throws(function () {
+            e1.set_matricula('14796');
+        }, TypeError);
+
+        assert.throws(function () {
+            e1.set_cpf('00000000000');
+        }, TypeError);
+    });
+
+    it('deve ser possível se matricular em uma turma', function () {
+        e1.matricula(t1);
+        e1.matricula(t2);
+        e1.matricula(t3);
+        assert.equal(3, e1.get_turmas().length);
+        e1.matricula(t3);
+        assert.equal(3, e1.get_turmas().length);
+    });
+
+    it('deve ser possível filtrar as turmas matriculadas por semestre', function () {
+        assert.deepStrictEqual([t1], e1.turmas('1'));
+        assert.deepStrictEqual([t2], e1.turmas('2'));
+        assert.deepStrictEqual([t3], e1.turmas('3'));
+    });
+});
